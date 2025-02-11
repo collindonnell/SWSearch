b586fab7bd3db70412d07c5755b5131d6521bfc5
Collin Donnell <collin@waveform.fm>
2025-02-11 14:58:34 -0800

    Initial Commit

---
+++ SWSearch/SWSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWSearchApp.swift
+//  SWSearch
+//
+//  Created by Collin Donnell on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SWSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SWSearchTests/SWSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  SWSearchTests.swift
+//  SWSearchTests
+//
+//  Created by Collin Donnell on 2/11/25.
+//
+
+import Testing
+@testable import SWSearch
+
+struct SWSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
