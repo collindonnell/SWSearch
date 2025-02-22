c1f657c8e80e010bea15e3291994b7ba96c963e2
Collin Donnell <collin@waveform.fm>
2025-02-21 18:51:25 -0800

    Add test for mass string

--- SWSearchTests/MetricValueFormatterTests.swift
+++ SWSearchTests/MetricValueFormatterTests.swift
@@ -11,7 +11,8 @@ import Foundation
 
 struct MetricValueFormatterTests {
 
-    @Test func testLengthFormattingFromCentimeters() async throws {
+    @Test("Formats localized human height string from centimeters")
+    func testHeightFormattingFromCentimeters() async throws {
         let formattedHeight = MetricValueFormatter.format(172, unitType: .height)
         if Locale.current.measurementSystem == .metric {
             #expect(formattedHeight.contains("cm"))
@@ -19,5 +20,15 @@ struct MetricValueFormatterTests {
             #expect(formattedHeight.contains("ft") || formattedHeight.contains("in"))
         }
         #expect(!formattedHeight.contains("yd"))
+    }
+
+    @Test("Formats localized human mass string from kilograms")
+    func testMassFormattingFromKilograms() async throws {
+        let formattedMass = MetricValueFormatter.format(72, unitType: .mass)
+        if Locale.current.measurementSystem == .metric {
+            #expect(formattedMass.contains("kg"))
+        } else {
+            #expect(formattedMass.contains("lb"))
+        }
     }
 }
